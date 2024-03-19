ac46b37f9dd75949154c9d4b5cafac4b507c0de3
LiloMarino <lilo.marino@hotmail.com>
2024-03-19 16:32:03 -0300

    first commit

---
+++ src/main/java/group/Prova1ConsumidorMuriloMarino/Prova1ConsumidorMuriloMarinoApplication.java
@@ -0,0 +1,13 @@
+package group.Prova1ConsumidorMuriloMarino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prova1ConsumidorMuriloMarinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prova1ConsumidorMuriloMarinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/group/Prova1ConsumidorMuriloMarino/Prova1ConsumidorMuriloMarinoApplicationTests.java
@@ -0,0 +1,13 @@
+package group.Prova1ConsumidorMuriloMarino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prova1ConsumidorMuriloMarinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
