345d1aba589a6129b1383841d205d6670f2f7adb
Murilo <lilo.marino@hotmail.com>
2024-03-23 15:39:22 -0300

    Adicionado página do pedido

---
+++ src/main/java/group/Prova1ConsumidorMuriloMarino/PedidoController.java
@@ -0,0 +1,14 @@
+package group.Prova1ConsumidorMuriloMarino;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PedidoController {
+
+    @GetMapping("/pedido")
+    public String mostrarPedido() {
+        return "pedido";
+    }
+}
